402e0a460464c23713fc872e98601ada0e917811
tommy-mor <thmorriss@gmail.com>
2022-07-23 23:46:02 +0000

    install script works

--- install.clj
+++ install.clj
@@ -1,2 +1,3 @@
 (require '[babashka.fs :as fs])
-(fs/copy-tree "addons/" "test_addons/" {:replace-existing true})
+(fs/copy-tree "addons/" "../tf/addons/" {:replace-existing true})
+(fs/copy-tree "maps/" "../tf/maps/" {:replace-existing true})

51c769cd0a2f9d4085ca1a3aa6b78cd9874a9ba2
tommy-mor <thmorriss@gmail.com>
2022-07-24 00:08:16 +0000

    install/compile script

--- install.clj
+++ install.clj
@@ -1,3 +1,8 @@
 (require '[babashka.fs :as fs])
+(require '[babashka.tasks :refer [shell]])
+
 (fs/copy-tree "addons/" "../tf/addons/" {:replace-existing true})
 (fs/copy-tree "maps/" "../tf/maps/" {:replace-existing true})
+
+(shell {:dir "../tf/addons/sourcemod/scripting/"} "./compile.sh")
+
